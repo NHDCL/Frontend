8ffe42a9ac7449a66104dedf7c1faf9047be8ada
12210010.gcit <12210010.gcit@rub.edu.bt>
2025-07-06 23:57:11 +0600

    done

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:8765";
-export const USERS_URL = "http://localhost:8765/USERMICROSERVICE/api";
+export const BASE_URL = "http://94.74.91.187:8765";
+export const USERS_URL = "/USERMICROSERVICE/api";
 
 export const ASSETS_URL = "/ASSETMICROSERVICE/api";
 export const MAINTENANCE_URL = "/MAINTENANCEMICROSERVICE/api";
